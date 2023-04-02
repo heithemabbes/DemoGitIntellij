adaf67d4cd403a1e0f59c940f4b308e19eaecccd
Heithem <heithem.abbes@gmail.com>
2023-04-02 14:22:19 +0100

    Init

---
+++ src/main/java/com/example/demogitintellij/DemoGitIntellijApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogitintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitIntellijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitIntellijApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demogitintellij/DemoGitIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogitintellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitIntellijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
