2845cc867ecacade0759b0143fe47c53eba73d8c
UTM-Heithem <129676091+UTM-Heithem@users.noreply.github.com>
2023-04-02 16:35:15 +0100

    Update DevClass
    Adding Some comments

--- src/main/java/com/example/demogitintellij/DevClass.java
+++ src/main/java/com/example/demogitintellij/DevClass.java
@@ -4,5 +4,7 @@ public class DevClass {
 
     public static void main(String[] args) {
         System.out.println("DevClass");
+        //UTM adding something here...
+        System.out.println("Adding code of UTM");
     }
 }
